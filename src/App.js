b3243935ed722ee367aab08f341bd6c85fc262aa
st1990pg <st1990pg@gmail.com>
2020-02-29 16:11:07 +0100

    push app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <h1>My react App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
