93d50d4c6fcb1a9119c8699fbebfeeb594eab485
st1990pg <st1990pg@gmail.com>
2020-03-02 19:43:36 +0100

    Prvi domoaci

---
+++ src/Components/ButtonArrow.js
@@ -0,0 +1,9 @@
+const ButtonArrow = props => {
+  return (
+    <div>
+      <button type="button"></button>
+    </div>
+  );
+};
+
+export default ButtonArrow;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require("path");
 const htmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "index_build.js"
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, "src"), "node_modules"]
   },
   module: {
     rules: [
@@ -15,12 +19,28 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[path][name].[ext]"
+          }
+        }
       }
     ]
   },
   plugins: [
     new htmlWebpackPlugin({
       template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "index.css"
     })
   ]
 };
