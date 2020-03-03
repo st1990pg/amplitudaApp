8e0f01b5866418765412e328259d9ca42adbaadd
Stefan Nikocevic <nikocevicstefan@gmail.com>
2020-03-03 09:19:21 +0100

    Testing push privileges

--- src/Components/ButtonRadius.js
+++ src/Components/ButtonRadius.js
@@ -1,6 +1,8 @@
 import React from "react";
 const ButtonRadius = props => {
-  return <div></div>;
+  return <div>
+    <h1>Hello from the button component</h1>
+  </div>;
 };
 
 export default ButtonRadius;
