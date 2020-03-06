f015776463de6a330856c6109132921371a505c7
st1990pg <st1990pg@gmail.com>
2020-03-06 18:47:07 +0100

    Merged contact section

--- src/Page/Home/index.js
+++ src/Page/Home/index.js
@@ -1,11 +1,16 @@
 import React from "react";
+import ContactUs from "./ContactUs";
 class Home extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <ContactUs />
+      </div>
+    );
   }
 }
 
