b52c9f1e80865ca7852058610d777e41028851d6
NinaaMaric <nina.maric@amplitudo.me>
2020-03-06 11:41:26 +0100

    icons added

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../../scss/footer.scss';
-import Djevojcica from "../../img/Djevojcica.png";
-import Djecak from "../../img/Djecak.png";
+import girl from "../../img/Djevojcica.png";
+import boy from "../../img/Djecak.png";
+import Icons from "./icons";
 
-function Footer() {
+const Footer = props =>{
   return (
     <div>
-      <img src={Djevojcica}  alt="djevojcica" className="djevojcica" /> 
-      <img src={Djecak} alt="djecak" className="djecak" />
+      <img src={girl}  alt="djevojcica" className="girl" /> 
+      <img src={boy} alt="djecak" className="boy" />
     <footer>
         <div className="mainFooter">
             <div className="container-fluid">
@@ -19,13 +20,18 @@ function Footer() {
                     NVO „Nacionalni osmijeh“ <br/>
                     <a target="_blank" href="#" title="mail" className="mail">
                       njihov.osmijeh@gmail.com</a>
-                      <hr/>
+                    <div className="row justify-content-center">
+                        <div className="col-6">
+                          <hr />
+                        </div>
+                    </div> 
                    <div className="mobile">
                     <p>067 330 137</p>
+                    <Icons/>
                     </div>
                   </div>
                 </div>
-            </div>  
+            </div> 
         </div>  
       </footer>
     </div>
---
+++ src/Components/Footer/icons.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Icons = props => {
+    return (
+        <div className="icons">
+            <a href="#"><i className="fab fa-facebook-square"></i></a>
+            <a href="#"><i className="fab fa-instagram"></i></a>
+        </div>
+    );
+}
+
+export default Icons;
