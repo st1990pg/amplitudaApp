d28bc2e031f177c7bc03a91c9cda765e4d8f13b8
NinaaMaric <nina.maric@amplitudo.me>
2020-03-06 12:45:32 +0100

    with logo

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import '../../scss/footer.scss';
+/* import '../../scss/Footer.scss'; */
 import girl from "../../img/Djevojcica.png";
 import boy from "../../img/Djecak.png";
 import Icons from "./icons";
+import Logo from "../Logo";
 
 const Footer = props =>{
   return (
@@ -12,16 +13,16 @@ const Footer = props =>{
     <footer>
         <div className="mainFooter">
             <div className="container-fluid">
-              <div className="row justify-content-center">
-                 <div className="logo"></div>
+              <div className="row justify-content-center logo">
+                <Logo />
               </div>
                 <div className="row justify-content-center">
-                  <div className="col-4">
+                  <div className="col-sm-6 col-lg-4">
                     NVO „Nacionalni osmijeh“ <br/>
                     <a target="_blank" href="#" title="mail" className="mail">
                       njihov.osmijeh@gmail.com</a>
                     <div className="row justify-content-center">
-                        <div className="col-6">
+                        <div className="col-xs-8 col-lg-6">
                           <hr />
                         </div>
                     </div> 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "index.scss";
-import "./scss/footer.scss";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById("root"));
