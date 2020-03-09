69b0a15fdfce7e5c751fbb92e8cfce6004f8a897
st1990pg <st1990pg@gmail.com>
2020-03-09 18:26:07 +0100

    V4 debugging

--- src/Components/ButtonRadius.js
+++ src/Components/ButtonRadius.js
@@ -1,19 +1,21 @@
 import React from "react";
-import '../scss/ButtonRadius.scss'
-import PropTypes from 'prop-types';
+import "../scss/ButtonRadius.scss";
+import PropTypes from "prop-types";
 
-
-const ButtonRadius = ({classes, text}) => {
-    return <button type="button" className={`ButtonRadius ${classes}`}>{text}</button>;
+const ButtonRadius = ({ classes, text }) => {
+  return (
+    <button type="button" className={`ButtonRadius ${classes}`}>
+      {text}
+    </button>
+  );
 };
 
 ButtonRadius.propTypes = {
-    classes: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired
+  text: PropTypes.string.isRequired
 };
 
 ButtonRadius.defaultProps = {
-    text: 'Pošalji'
+  text: "Pošalji"
 };
 
 export default ButtonRadius;
