744c354805841f61de14e1ef1767857b01abb9fc
tamara-jokic <tamara-jokic@users.noreply.github.com>
2020-03-12 22:32:30 +0100

    About.scss

--- src/Page/Home/About.js
+++ src/Page/Home/About.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import Image from './photo.jpg';
-import Wave from './Path15.svg';
+import Image from '../../img/photo.jpg';
+import Wave from '../../img/Path15.svg';
 import ButtonArrow from '../../../Components/ButtonArrow';
 
-import './About.css';
 
 const About = props =>{
 
--- src/Page/Home/index.js
+++ src/Page/Home/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import About from './About/About';
+import About from './About';
 import ContactUs from "./ContactUs";
 import SimpleSlider from "./Slider";
 
